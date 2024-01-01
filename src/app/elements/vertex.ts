d7acaf949988352edd854aff9be7dce7666fce6c
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-01-01 17:25:50 -0500

    started vertex class

---
+++ src/app/elements/vertex.ts
@@ -0,0 +1,13 @@
+class Vertex {
+    x: number;
+    y: number;
+    label: string;
+
+    constructor(x: number, y: number, label: string) {
+        this.x = x;
+        this.y = y;
+        this.label = label;
+    }
+}
+
+export default Vertex;
