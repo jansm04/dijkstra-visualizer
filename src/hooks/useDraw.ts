f56a508bdf053d0dfdbe33a484db367392964da4
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-01-01 18:17:30 -0500

    add vertex event handler

--- src/hooks/useDraw.ts
+++ src/hooks/useDraw.ts
@@ -1,19 +1,39 @@
 import { useEffect, useRef } from "react";
 
 export const useDraw = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null)
+    const canvasRef = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
         const handler = (e: MouseEvent) => {
-            console.log({x: e.clientX, y: e.clientY})
+            console.log({x: e.clientX, y: e.clientY});
+            const point = computePointInCanvas(e);
+            if (!point) return;
+
+            // draw circle
+            const ctx = canvasRef.current?.getContext("2d");
+            if (!ctx) return;
+            ctx.beginPath();
+            ctx.arc(point.x, point.y, 40, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+
+        const computePointInCanvas = (e: MouseEvent) => {
+            const canvas = canvasRef.current;
+            if (!canvas) return;
+
+            const rect = canvas.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            return {x, y};
         }
 
         // add event listeners
-        canvasRef.current?.addEventListener('dblclick', handler)
+        canvasRef.current?.addEventListener('dblclick', handler);
 
         // remove event listeners
-        return () => canvasRef.current?.addEventListener('dblclick', handler)
+        return () => canvasRef.current?.addEventListener('dblclick', handler);
     }, [])
 
-    return { canvasRef }
+    return { canvasRef };
 }
