7b08620e1b98be10a3721192cf044f491b90304b
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-01-01 22:48:18 -0500

    event listeners for edges

--- src/hooks/useDraw.ts
+++ src/hooks/useDraw.ts
@@ -1,10 +1,17 @@
 import Vertex from "@/app/elements/vertex";
+import Edge from "@/app/elements/edge";
+
 import { useEffect, useRef } from "react";
+import TempEdge from "@/app/elements/temp-edge";
 
 export const useDraw = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     var vertices = new Array<Vertex>();
+    var edges = new Array<Edge>();
+    var tempEdge: TempEdge | null;
+
     var selectedObject: Vertex | null = null;
+    var isShiftPressed = false;
     var count = 0;
 
     useEffect(() => {
@@ -13,7 +20,7 @@ export const useDraw = () => {
             return;
         } else count++;
 
-        const handler = (e: MouseEvent) => {
+        const onDoubleClick = (e: MouseEvent) => {
             const point = computePointInCanvas(e);
             if (!point) return;
 
@@ -23,8 +30,63 @@ export const useDraw = () => {
                 selectedObject = vertex;
                 vertices.push(vertex);
             }
-            draw(canvasRef.current?.getContext("2d"));
+            drawGraph(canvasRef.current?.getContext("2d"));
             console.log(vertices);
+        }
+
+        const onClick = (e: MouseEvent) => {
+            const point = computePointInCanvas(e);
+            if (!point) return;
+            selectedObject = selectObject(point.x, point.y);
+            if (selectedObject) {
+                drawGraph(canvasRef.current?.getContext("2d"));
+            }
+        }
+
+        const onMouseDown= (e: MouseEvent) => {
+            var point = computePointInCanvas(e);
+            if (!point) return;
+
+            selectedObject = selectObject(point.x, point.y);
+            console.log(selectedObject);
+
+            if (selectedObject && isShiftPressed) {
+                tempEdge = new TempEdge(selectedObject, point.x, point.y);
+            }
+        }
+
+        const onMouseMove = (e: MouseEvent) => {
+            if (tempEdge) {
+                var point = computePointInCanvas(e);
+                if (!point) return;
+                tempEdge.px = point.x;
+                tempEdge.py = point.y;
+                drawGraph(canvasRef.current?.getContext("2d"));
+            }
+        }
+
+        const onMouseUp= (e: MouseEvent) => {
+            const point = computePointInCanvas(e);
+            if (!point) return;
+            selectedObject = selectObject(point.x, point.y);
+            if (selectedObject && tempEdge) {
+                var edge = new Edge(0, selectedObject, tempEdge.vertex);
+                edges.push(edge);
+            }
+            tempEdge = null;
+            drawGraph(canvasRef.current?.getContext("2d"));
+        }
+
+        const onKeyDown = (e: KeyboardEvent) => {
+            if (e.key == 'Shift') {
+                isShiftPressed = true;
+            }
+        }
+
+        const onKeyUp = (e: KeyboardEvent) => {
+            if (e.key == 'Shift') {
+                isShiftPressed = false;
+            }
         }
 
         const computePointInCanvas = (e: MouseEvent) => {
@@ -46,24 +108,47 @@ export const useDraw = () => {
             return null;
         }
 
-        const draw = (ctx: CanvasRenderingContext2D | null | undefined) => {
+        const drawGraph = (ctx: CanvasRenderingContext2D | null | undefined) => {
             if (!ctx) return;
-            
+
             const rect = canvasRef.current?.getBoundingClientRect();
             if (!rect) return;
             ctx?.clearRect(0, 0, rect.width, rect.height);
 
+            // draw vertices
             for (let i = 0; i < vertices.length; i++) {
                 ctx.strokeStyle = (vertices[i] == selectedObject) ? 'blue' : 'black';
                 vertices[i].draw(ctx);
             }
+
+            // draw temp edge
+            if (tempEdge) {
+                for (let i = 0; i < vertices.length; i++) {
+                    if (vertices[i].containsPoint(tempEdge.px, tempEdge.py)) break;
+                }
+                tempEdge.draw(ctx);
+            } 
         }
 
         // add event listeners
-        canvasRef.current?.addEventListener('dblclick', handler);
+        canvasRef.current?.addEventListener('dblclick', onDoubleClick);
+        canvasRef.current?.addEventListener('click', onClick);
+        canvasRef.current?.addEventListener('mousedown', onMouseDown);
+        canvasRef.current?.addEventListener('mousemove', onMouseMove);
+        canvasRef.current?.addEventListener('mouseup', onMouseUp);
+        canvasRef.current?.addEventListener('keydown', onKeyDown, true);
+        canvasRef.current?.addEventListener('keyup', onKeyUp, true);
 
         // remove event listeners
-        return () => canvasRef.current?.addEventListener('dblclick', handler);
+        return () => {
+            canvasRef.current?.addEventListener('dblclick', onDoubleClick);
+            canvasRef.current?.addEventListener('click', onClick);
+            canvasRef.current?.addEventListener('mousedown', onMouseDown);
+            canvasRef.current?.addEventListener('mousemove', onMouseMove);
+            canvasRef.current?.addEventListener('mouseup', onMouseUp);
+            canvasRef.current?.addEventListener('keydown', onKeyDown, true);
+            canvasRef.current?.addEventListener('keyup', onKeyUp, true);
+        }
     }, [])
 
     return { canvasRef };
