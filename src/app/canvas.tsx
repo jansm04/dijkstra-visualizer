fdb67a1754f7b697f0e69371cb52192b56eb3c9e
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-01-05 13:13:14 -0500

    added pq canvas

--- src/app/canvas.tsx
+++ src/app/canvas.tsx
@@ -1,21 +1,26 @@
 'use client'
 import { useDraw } from "@/hooks/useDraw"
 import Header from '@/components/header'
-import Footer from '@/components/footer'
 import Instructions from "@/components/instructions"
 
 export const Canvas = () => {
 
-    const { canvasRef } = useDraw()
+    const { canvasRef, pqRef, startRef } = useDraw()
 
     return (
         <div>
             <Header />
             <div className="flex">
+                 <div>
+                    <h1 className="m-2 text-gray-400 text-[16px] text-center font-bold">Priority Queue</h1>
+                    <canvas ref={pqRef} height={500} width={200} className="my-4 mx-12 border border-gray-400 outline-none max-w-[400px]" tabIndex={0}></canvas>
+                 </div>
                 <canvas ref={canvasRef} height={600} width={800} className="border outline-none border-white bg-slate-950 rounded-md max-w-[800px]" tabIndex={0}></canvas>
                 <Instructions />
             </div>
-            <Footer />
+            <div className="text-center">
+                <button ref={startRef} className="py-2 px-6 m-10 text-gray-300 italic text-center border border-white rounded hover:bg-slate-800">Visualize Algorithm</button>
+            </div>
         </div>
             
     )
