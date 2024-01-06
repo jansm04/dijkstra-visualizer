e66371604c891e3ab8c605489e4b1b83a631b11f
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-01-06 17:41:40 -0500

    added update pq visualizer

--- src/draw/draw_pq.ts
+++ src/draw/draw_pq.ts
@@ -1,27 +1,61 @@
 import PriorityQueue from "@/app/elements/priority_queue";
+import Vertex from "@/app/elements/vertex";
 
 import { RefObject } from "react";
 
+const regular = "h-12 text-center border border-gray-300 text-[14px]";
+const min = "h-12 text-center border border-yellow-500 text-[14px]"
+
 export const addPQVisualizer = (
-        pqRef: RefObject<HTMLTableElement>,
-        startVisRef: RefObject<HTMLButtonElement>,
-        pq: PriorityQueue
-    ) => {
-        
-        function onSubmitBuild(e: MouseEvent) {
-            if (!pqRef.current) return;
-            for (let i = 0; i < pq.vertices.length; i++) {
-                var row = pqRef.current.insertRow();
-                row.className = "h-8 text-center border border-gray-300 text-[13px]"
-                row.insertCell().textContent = pq.vertices[i].label;
-                if (pq.vertices[i].dist == Infinity)
-                    row.insertCell().textContent = "Inf";
-                else 
-                    row.insertCell().textContent = pq.vertices[i].dist.toString();
-                row.insertCell().textContent = pq.vertices[i].pred;
-            }
-        }
-    
-        if (!startVisRef.current) return;
-        startVisRef.current.addEventListener('click', onSubmitBuild);
+    pqRef: RefObject<HTMLTableElement>,
+    pq: PriorityQueue,
+    visited: Array<Vertex>
+) => {
+
+    if (!pqRef.current) return;
+    for (let i = 0; i < pq.vertices.length; i++) {
+        var row = pqRef.current.insertRow();
+        row.className = regular;
+        row.insertCell().textContent = pq.vertices[i].label;
+        if (pq.vertices[i].dist == Infinity)
+            row.insertCell().textContent = "Inf";
+        else 
+            row.insertCell().textContent = pq.vertices[i].dist.toString();
+        row.insertCell().textContent = pq.vertices[i].pred;
+    }
+}
+
+export const updatePQVisualizer = (
+    pqRef: RefObject<HTMLTableElement>,
+    pq: PriorityQueue,
+    visited: Array<Vertex>
+) => {
+
+    let idx = 1;
+    visited.forEach((vertex) => {
+        if (!pqRef.current) return;
+        var row = pqRef.current.rows[idx];
+        row.className = regular + " text-gray-400";
+        row.cells[0].textContent = vertex.label;
+        if (vertex.dist == Infinity)
+            row.cells[1].textContent = "Inf";
+        else 
+            row.cells[1].textContent = vertex.dist.toString();
+        row.cells[2].textContent = vertex.pred;
+        idx++;
+    })
+
+    pq.vertices.forEach((vertex) => {
+        if (!pqRef.current) return;
+        var row = pqRef.current.rows[idx];
+        row.className = vertex == pq.front() ? min : regular;
+        row.cells[0].textContent = vertex.label;
+        if (vertex.dist == Infinity)
+            row.cells[1].textContent = "Inf";
+        else 
+            row.cells[1].textContent = vertex.dist.toString();
+        row.cells[2].textContent = vertex.pred;
+        idx++;
+    })
+
 }
