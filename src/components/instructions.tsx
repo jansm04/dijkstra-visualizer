9509ee6fd0f5006a618dc81fc3dc2da6c4bb31db
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-01-07 22:50:57 -0500

    fixed instructions width

--- src/components/instructions.tsx
+++ src/components/instructions.tsx
@@ -1,6 +1,6 @@
 const Instructions = () => {
     return (
-        <div className="mx-8 mt-12 text-gray-400 max-w-[240px] text-[14px]">
+        <div className="mx-8 mt-12 text-gray-400 w-[240px] text-[14px]">
             <h1 className="mb-8">Use the canvas to build your graph.</h1>
             <div className="mb-2">
                 <p className="float-left text-gray-300 mr-2 font-bold">To add a vertex:</p>
