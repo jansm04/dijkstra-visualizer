e8b5ebd913e3c22058c6599f2cca0450b8d73ef6
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-01-08 20:25:18 -0500

    bolded header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <header>
-            <h1 className="mt-[4vh] mb-4 text-center text-3xl italic">
+            <h1 className="mt-[4vh] mb-4 text-center text-3xl font-semibold text-gray-800 italic">
                 Dijkstra&apos;s Algorithm Visualizer
             </h1>
             <p className="mt-1 mb-6 text-gray-500 text-center text-[14px]">
