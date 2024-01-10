2996d5c27ab632283c80bd237f33ecbbfa4b0355
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-01-09 23:54:06 -0500

    added min width property

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Canvas } from './canvas'
 
 export default function Home() {
   return (
-    <main className='m-2 h-screen w-screen flex justify-center'>
+    <main className='m-2 min-w-[1200px] h-screen w-screen flex justify-center'>
       <Canvas />
     </main>
   )
