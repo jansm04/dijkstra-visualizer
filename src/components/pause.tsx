8d4afd44007461c74b0bc18ff2288213c26825b8
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-01-15 11:11:19 -0500

    style fix

--- src/components/pause.tsx
+++ src/components/pause.tsx
@@ -8,7 +8,7 @@ const Pause = ({
 }) => {
     return (
         <div className="mx-8 w-[180px]">
-            <button ref={refs.pauseRef} className="mt-4 py-1 px-3 w-fit italic border border-gray-800 text-center text-[13px] rounded hover:bg-amber-400" hidden={true}>
+            <button ref={refs.pauseRef} className="mt-4 py-1 px-3 w-fit italic border border-gray-800 text-center text-gray-700 text-[13px] rounded hover:bg-amber-400" hidden={true}>
                 Pause
             </button>
         </div>
