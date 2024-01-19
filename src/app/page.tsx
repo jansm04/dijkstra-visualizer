8d3cdddaa6d06ee113be5b2903e26be02298695c
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-01-18 20:51:21 -0500

    page style fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Main } from '../components/main'
 
 export default function Home() {
   return (
-    <main className='m-2 min-w-[1200px] h-screen w-screen flex justify-center'>
+    <main className='m-2 min-w-[1200px] flex justify-center'>
       <Main />
     </main>
   )
