fa545d5fe15e865c27f0ee4da03630e58822de98
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-02-08 20:52:01 -0500

    added github link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,11 +3,10 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <div className="mt-6 p-6 text-gray-500 text-center text-[13px]">
-            Made by 
-                {<Link href='https://github.com/jansm04/dav' target="_blank">
-                    <div className="text-sky-700 inline"> @jansm04 </div>
-                </Link>} 
-            in 2024
+            Made by @jansm04 in 2024&nbsp; •&nbsp; 
+            {<Link href='https://github.com/jansm04/dav' target="_blank">
+                <div className="text-sky-700 inline"> Github </div>
+            </Link>}
         </div>
     )
 }
