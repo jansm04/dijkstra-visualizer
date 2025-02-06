91d19e48c612b98493f5782e7b763d12ff8fbe08
jansm04 <jan.smailbegovic@gmail.com>
2025-02-05 21:52:05 -0800

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,13 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <div className="mt-6 p-6 text-gray-500 text-center text-[13px]">
-            Made by @jansm04 in 2024&nbsp; •&nbsp; 
+            Made by {
+                <Link href="https://www.janbegovic.com/" target="_blank">
+                    <div className="text-sky-600 inline"> Jan Smailbegovic </div>
+                </Link>
+            } in 2024&nbsp; •&nbsp; 
             {<Link href='https://github.com/jansm04/dav' target="_blank">
-                <div className="text-sky-700 inline"> Github </div>
+                <div className="text-sky-600 inline"> GitHub </div>
             </Link>}
         </div>
     )
